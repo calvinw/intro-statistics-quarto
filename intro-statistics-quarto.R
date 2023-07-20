6a26d2c46ae97fe3f0325cec89a1b30e916bd022
calvinw <calvin.e.williamson@gmail.com>
2023-07-20 07:43:31 -0400

    more chapters

--- intro-statistics-quarto.R
+++ intro-statistics-quarto.R
@@ -2,12 +2,15 @@ library(knitr)
 
 knitr::opts_chunk$set(echo = FALSE)
 
+example_label<-function(label) {
+}
+
 knit_example<-function(params, question, solution) {
   env<-knit_global()
   env$params <- params
 #  env$question <- paste0("statplosion-problems/question/", question)
 #  env$solution <- paste0("statplosion-problems/solution/", solution)
-  env$question <- question 
+  env$question <- question
   env$solution <- solution
   asis_output(knit_child("example.Rmd", quiet=T))
 }

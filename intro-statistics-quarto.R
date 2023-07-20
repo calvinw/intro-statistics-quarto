ed1321c699d50646b448e145d2ec887bcd630121
calvinw <calvin.e.williamson@gmail.com>
2023-07-19 20:22:20 -0400

    more on the examples

--- intro-statistics-quarto.R
+++ intro-statistics-quarto.R
@@ -5,8 +5,10 @@ knitr::opts_chunk$set(echo = FALSE)
 knit_example<-function(params, question, solution) {
   env<-knit_global()
   env$params <- params
-  env$question <- paste0("statplosion-problems/question/", question)
-  env$solution <- paste0("statplosion-problems/solution/", solution)
+#  env$question <- paste0("statplosion-problems/question/", question)
+#  env$solution <- paste0("statplosion-problems/solution/", solution)
+  env$question <- question 
+  env$solution <- solution
   asis_output(knit_child("example.Rmd", quiet=T))
 }
 
